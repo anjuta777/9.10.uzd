93fd5c57b4d4ea6871265f8a7e71a0fa3ecdb0ec
akibalnika <77092253+anjuta777@users.noreply.github.com>
2022-02-24 12:43:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+a = int(input("Ievadiet cilveku skaitu: "))
+b = int(input("Ievadiet 1 cilveka masu: "))
+c = int(input("Ievadiet 2. cilveka masu: "))
+d = int(input("Ievadiet 3. cilveka masu: "))
+e = int(input("Ievadiet 4. cilveka masu: "))
+f = b + c + d + e
+g = f/a
+print(g)
+
+x = 300
+if f <= x:
+  print("Jums varat braukt lifta")
+else:
+  print("Jums nedrikst braukt lifta")
